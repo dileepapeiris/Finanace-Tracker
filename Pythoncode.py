71bb39e4b2a86be18202aff904630feb3e745802
K.Dileepa Thushan Peiris <162027037+dileepapeiris@users.noreply.github.com>
2024-12-19 12:51:15 +0530

    Import messagebox for displaying messages

--- Pythoncode.py
+++ Pythoncode.py
@@ -1,2 +1,3 @@
 import tkinter as tk # Import the tkinter module for GUI
 from tkinter import ttk # Import themed tkinter widgets
+from tkinter import messagebox  # Import messagebox for displaying messages
