9a7abb34d5313a7938daa0541ec84a356270fb24
dileepapeiris <dileepapeiris5@gmail.com>
2024-12-19 13:33:11 +0530

    Import json module for JSON handling

--- Pythoncode.py
+++ Pythoncode.py
@@ -2,5 +2,6 @@ import tkinter as tk # Import the tkinter module for GUI
 from tkinter import ttk # Import themed tkinter widgets
 from tkinter import messagebox  # Import messagebox for displaying messages
 import datetime  # Import datetime module for date and time operations
+import json  # Import json module for JSON handling
 
 
